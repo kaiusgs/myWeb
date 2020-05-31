4aa657fe7b8d85ebec1c4f9a4cc1344b2233e41d
kaiusgs <1529230703@qq.com>
2020-05-31 23:46:31 +0800

    version3

--- app.py
+++ app.py
@@ -23,7 +23,36 @@ def game1():
     t = int( time.strftime("%H", time.localtime()) )
     if t>=5 and t<20:
         return render_template('Nogame.html')
-    return render_template('game.html')
+    return render_template('game1.html')
+
+@app.route('/drink/game2')
+def game2():
+    t = int( time.strftime("%H", time.localtime()) )
+    if t>=5 and t<20:
+        return render_template('Nogame.html')
+    return render_template('game2.html')
+
+@app.route('/drink/game3')
+def game3():
+    t = int( time.strftime("%H", time.localtime()) )
+    if t>=5 and t<20:
+        return render_template('Nogame.html')
+    return render_template('game3.html')
+
+@app.route('/drink/game4')
+def game4():
+    t = int( time.strftime("%H", time.localtime()) )
+    if t>=5 and t<20:
+        return render_template('Nogame.html')
+    return render_template('game4.html')
+
+@app.route('/drink/game5')
+def game5():
+    t = int( time.strftime("%H", time.localtime()) )
+    if t>=5 and t<20:
+        return render_template('Nogame.html')
+    return render_template('game5.html')
+
 
 @app.route('/love')
 def love():
